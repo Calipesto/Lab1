be29f5a4066f84958de77e76fcc40b21a26f1acf
Виктория <calipesto@inbox.ru>
2020-12-11 17:29:16 +0300

    Первый коммит

---
+++ index.php
@@ -0,0 +1,16 @@
+﻿<!DOCTYPE htm1>
+<html>
+	<head>
+		<title> Моя страничка </title>
+		<link rel="stylesheet" href="mycss.css">
+		<link rel="shortcut icon" href="favicon.ico">
+	</head>
+	<body>
+		<div class="mydiv1">
+			<?php
+				echo "Площадь квадрата со стороной a=4, равна ";
+				echo 4*4;
+			?>
+		</div>
+	</body>
+</html>
